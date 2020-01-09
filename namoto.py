cf1a72b368533b50c56c2570eaa02e62c289276c
JQ <test@jq.com>
2020-01-08 18:26:38 -0800

    Created add_to_block and get_last_block functions

--- namoto.py
+++ namoto.py
@@ -1,2 +1,12 @@
 # Initialized the namoto block
 namoto_block = []
+
+# Add transaction to the block
+def add_to_block(transaction_amount, last_transaction=[1]):
+    
+    namoto_block.append([last_transaction, transaction_amount])
+
+# Get last transaction of our block
+def get_last_block():
+    """ Returns last blockchain value """
+    return namoto_block[-1]
