f7821e0ffcc6feb0a88f0d646b1521d823668d0d
JQ <test@jq.com>
2020-01-11 16:46:03 -0800

    Added block_length condition check to functions

--- namoto.py
+++ namoto.py
@@ -1,12 +1,19 @@
 # Initialized the namoto block
 namoto_block = []
+block_length = len(namoto_block)
 
 # Add transaction to the block
 def add_to_block(transaction_amount, last_transaction=[1]):
-    
+
+     if block_length < 1:
+        last_transaction = 0
+
     namoto_block.append([last_transaction, transaction_amount])
 
 # Get last transaction of our block
 def get_last_block():
     """ Returns last blockchain value """
+    if block_length < 1:
+        return None
+
     return namoto_block[-1]
